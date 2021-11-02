51dca2351e7e69ebd8968af60d43ae9639f6156a
Florian FAYEL <ffayel@gmail.com>
2021-11-02 17:17:15 +0100

    subscribe to store and dispatch init

--- latihan/src/App.js
+++ latihan/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ViewLogin from './components/view/ViewLogin'
+import ActionTypes from './logic/pattern/actions/action-types';
+
+import store from './logic/pattern/store'
+
+store.subscribe(()=> console.log(store.getState()));
+store.dispatch({type: ActionTypes.APP_INITIALIZED, payload:null});
+
 
 function App() {
   return (

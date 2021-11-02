49f083421a98d3ac164ef584ad8d9171d1ca4c61
Florian FAYEL <ffayel@gmail.com>
2021-11-02 15:41:43 +0100

    afficher plus clairement les fichiers charger lors du lazyLoad

--- latihan/src/components/view/ViewLogin/ViewLogin.lazy.js
+++ latihan/src/components/view/ViewLogin/ViewLogin.lazy.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 
-const LazyViewLogin = lazy(() => import('./ViewLogin'));
+// permet de renomé le fichier qui sera cahrger dans le network
+const LazyViewLogin = lazy(() => import(/* webpackChunkName: "view-login"*/'./ViewLogin'));
 
 const ViewLogin = props => (
   <Suspense fallback={null}>

30c8d72fecc78646f0494f015b72e05519decbc9
Florian FAYEL <ffayel@gmail.com>
2021-11-03 17:08:47 +0100

    use state et useEffect

--- latihan/src/components/feature/FeatureLogin/FeatureLogin.js
+++ latihan/src/components/feature/FeatureLogin/FeatureLogin.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './FeatureLogin.css';
 import { BaseButton, BaseInput } from '../../base';
 
-const FeatureLogin = () => (
+const FeatureLogin = () => {
+
+  const [credentials, setCredentials] = React.useState(
+    {username:'', password:''}
+  );
+
+  //HOF : Higher Order Function
+  const inputChangeHandler = val => e => {
+    setCredentials({ ...credentials, ...{ [val]: e.target.value } })
+  }
+
+  //lifecycle
+  useEffect(
+    function initial (){ 
+      // componenDidMount : A faire à la montée du composant
+      const globalListener = e => console.warn('Global Click')
+
+      window.addEventListener('click', globalListener );
+
+      return function cleanup(){
+      // componenWillUnMount : A faire lorsques que le composant est démonté
+        window.removeEventListener('click', globalListener );
+      }
+    }
+  , [/* shouldcomponentUpdate : conditions de rédelcancehement de l'effet */] )
+
+
+  return (
   <div className="FeatureLogin" data-testid="FeatureLogin">
-      <BaseInput label="controlled"></BaseInput>
-      <BaseInput label="uncontrolado" value={null}></BaseInput>
+      <BaseInput 
+        label="UserName" 
+        value={credentials.username} onChange={inputChangeHandler('username')} 
+      />
+      <BaseInput 
+        label="Password" 
+        value={credentials.password} onChange={inputChangeHandler('password')} 
+      />
+      {/* <BaseInput label="uncontrolado" value={null}></BaseInput> */}
       <BaseButton></BaseButton>
   </div>
-);
+  )
+};
 
 FeatureLogin.propTypes = {};
 

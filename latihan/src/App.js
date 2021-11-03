a16d2f2fa1255b9c17175fa19f0484399e6e7302
Florian FAYEL <ffayel@gmail.com>
2021-11-03 16:09:26 +0100

    usage of state

--- latihan/src/App.js
+++ latihan/src/App.js
@@ -10,9 +10,44 @@ store.dispatch({type: ActionTypes.APP_INITIALIZED, payload:null});
 
 
 class App extends React.Component {
+
+  state = {}
+  time = Date.now();
+  timer = null;
+
+  tick(){
+    this.time = Date.now();
+    console.log(this.time);
+    this.setState({});
+  }
+
+  // Permet d'ignorer les alert ESLINT --->
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props){
+    super(props);
+  }
+  
+  componentDidMount(){
+    this.timer = setInterval( () => this.tick(), 1000);
+  }
+
+  getSnapshotBeforeUpdate(){}
+  componentDidUpdate(){}
+
+  //doit etre mis a jours si retourne true
+  shouldComponentUpdate(nextProps, nextState){
+    return true;
+  }
+
+  componentWillUnmount(){
+    this.timer && clearInterval(this.timer);
+  }
+
+
   render(){
     return (
       <React.Fragment>
+        <h1>{this.time}</h1>
         <ViewLogin></ViewLogin>
       </React.Fragment>
     );
